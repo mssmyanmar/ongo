21e2f494b4c36292394ceef42d16461cf8b54947
minpikehmu <mph@mssmyanmar.com>
2022-07-25 10:59:06 +0630

    login with staff and agent

--- app/Http/Controllers/api/v1/LoginController.php
+++ app/Http/Controllers/api/v1/LoginController.php
@@ -16,17 +16,22 @@ class LoginController extends Controller
     public function login(LoginRequest $request){
             $user = User::where('phone_number',$request->phone_number)->where('password',$request->password)->where('active_status',1)->first();
             if($user!=null){
-                $accesstoken = $user->createToken('authToken')->accessToken;
-                $mytime = Carbon::now();
-                return response(["response_code"    => "200200",
-                                 "status"           => "SUCCESS", 
-                                 "status_message"   => "Fetching staff info is successful!", 
-                                 "internal_message" => "Fetching staff info is successful!", 
-                                 "date_time_utc"    => $mytime, 
-                                 "data"             => [
-                                 "staff"            => $user,
-                                 "accesstoken"      => $accesstoken
-                                 ]]);       
+                $role = $user->getRoleNames();
+                if($role[0]=="staff" || $role[0]=="agent"){
+                    $accesstoken = $user->createToken('authToken')->accessToken;
+                    $mytime = Carbon::now();
+                    return response(["response_code"    => "200200",
+                                     "status"           => "SUCCESS", 
+                                     "status_message"   => "Fetching staff info is successful!", 
+                                     "internal_message" => "Fetching staff info is successful!", 
+                                     "date_time_utc"    => $mytime, 
+                                     "data"             => [
+                                     "staff"            => $user,
+                                     "accesstoken"      => $accesstoken
+                                     ]]); 
+                }else{
+                    return response(['message'=>'Invalid Phone number and password!'],422);
+                }    
             }else{
                 return response(['message'=>'Invalid Phone number and password!'],422);
             }
